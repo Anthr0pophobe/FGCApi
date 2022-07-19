025e7b341df41303d05c0aa9aba798ea03986379
Anthropohpobe <anthropophobe@MacBook-Air-de-Anthropohpobe.local>
2022-07-19 20:27:14 +0200

    Adding create and update user

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -30,7 +30,6 @@ const show = async (req, res) => {
 	const {
 		params: { userId },
 	} = req;
-	console.log(userId);
 	try {
 		const user = await prisma.users.findUnique({
 			where: {
@@ -52,12 +51,53 @@ const show = async (req, res) => {
 		return res.json({ succes: 'false', data: { error } });
 	}
 };
-// const create = async (req, res) => {};
-// const update = async (req, res) => {};
+
+const create = async (req, res) => {
+	try {
+		const { body } = req;
+		const createUser = await prisma.users.create({
+			data: {
+				...body,
+			},
+		});
+		return res.json({
+			succes: 'true',
+			data: createUser,
+			code: 200,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.json({ succes: 'false', data: { error } });
+	}
+};
+const update = async (req, res) => {
+	const {
+		params: { userId },
+	} = req;
+	const { body } = req;
+	try {
+		const updateUser = await prisma.users.update({
+			where: {
+				id: parseInt(userId, 10),
+			},
+			data: {
+				...body,
+			},
+		});
+		return res.json({
+			succes: 'true',
+			data: updateUser,
+			code: 200,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.json({ succes: 'false', data: { error } });
+	}
+};
 
 module.exports = {
 	index,
 	show,
-	// create,
-	// update,
+	create,
+	update,
 };
